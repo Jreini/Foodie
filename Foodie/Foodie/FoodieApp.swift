980aa29b81552472aed333a738d45161e7cfcad7
Justin Reini <jr6email@gmail.com>
2026-04-09 23:13:58 -0500

    feat: initial commit

---
+++ Foodie/Foodie/FoodieApp.swift
@@ -0,0 +1,21 @@
+//
+//  FoodieApp.swift
+//  Foodie
+//
+//  Created by Justin Reini on 4/9/26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct FoodieApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
